2352c6beb514c1ef1c554ef5927666e813d52ef4
lukas jonusis <lukas.jonusis@proit.lt>
2023-11-20 17:38:21 +0200

    Added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,42 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Birt Docs",
+      customCss: [
+        // Path to Tailwind base styles:
+        "./src/tailwind.css",
+      ],
+      social: {
+        github: "https://github.com/LJonn/birt-docs",
+      },
+      sidebar: [
+        {
+          label: "How-to",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "PostgreSQL in docker container",
+              link: "/guides/example/",
+            },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+    }),
+    tailwind({
+      // Disable the default base styles:
+      applyBaseStyles: false,
+    }),
+  ],
 });
